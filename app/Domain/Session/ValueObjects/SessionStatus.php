7202ffd3b801b53caef4fbc7046730121986b512
Kevin Guevara <kevinguevara02@gmail.com>
2026-07-07 06:05:10 -0500

    feat(domain): implement session status value object

---
+++ app/Domain/Session/ValueObjects/SessionStatus.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Session\ValueObjects;
+
+enum SessionStatus: string
+{
+    case Scheduled = 'scheduled';
+    case Completed = 'completed';
+    case Cancelled = 'cancelled';
+
+    public static function default(): self
+    {
+        return self::Scheduled;
+    }
+
+    public function isScheduled(): bool
+    {
+        return $this === self::Scheduled;
+    }
+
+    public function isCompleted(): bool
+    {
+        return $this === self::Completed;
+    }
+
+    public function isCancelled(): bool
+    {
+        return $this === self::Cancelled;
+    }
+}
---
+++ tests/Unit/Domain/Session/ValueObjetcs/SessionStatusTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Domain\Session\ValueObjects;
+
+use App\Domain\Session\ValueObjects\SessionStatus;
+use PHPUnit\Framework\TestCase;
+
+final class SessionStatusTest extends TestCase
+{
+    public function test_default_status_is_scheduled(): void
+    {
+        $this->assertSame(
+            SessionStatus::Scheduled,
+            SessionStatus::default()
+        );
+    }
+
+    public function test_it_knows_when_it_is_scheduled(): void
+    {
+        $this->assertTrue(
+            SessionStatus::Scheduled->isScheduled()
+        );
+
+        $this->assertFalse(
+            SessionStatus::Completed->isScheduled()
+        );
+    }
+
+    public function test_it_knows_when_it_is_completed(): void
+    {
+        $this->assertTrue(
+            SessionStatus::Completed->isCompleted()
+        );
+
+        $this->assertFalse(
+            SessionStatus::Cancelled->isCompleted()
+        );
+    }
+
+    public function test_it_knows_when_it_is_cancelled(): void
+    {
+        $this->assertTrue(
+            SessionStatus::Cancelled->isCancelled()
+        );
+
+        $this->assertFalse(
+            SessionStatus::Scheduled->isCancelled()
+        );
+    }
+}
